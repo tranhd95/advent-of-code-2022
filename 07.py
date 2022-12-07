8c46681ea045931cf1a75205b19745e67f53b1f3
Hai Duong Tran <tranhd95@gmail.com>
2022-12-07 18:10:39 +0100

    Rewrite Day 07 to pattern matching.

--- 07.py
+++ 07.py
@@ -7,29 +7,25 @@ def solve(inp):
     curr_dir = ("/",)
     fs = defaultdict(int)
     for line in inp.split("\n"):
-        if line.startswith("dir") or line.startswith("ls"):
-            continue
-        elif line.startswith("$"):
-            chunks = line.split(" ")
-            cmd = chunks[1]
-            if cmd == "cd":
-                arg = chunks[2]
-                if arg == "..":
-                    curr_dir = curr_dir[:-1]
-                elif arg == "/":
-                    curr_dir = ("/",)
-                else:
-                    curr_dir = curr_dir + (arg,)
-        else:
-            size, _ = line.split(" ")
-            size = int(size)
-            fs[curr_dir] += size
-            for i in range(1, len(curr_dir)):
-                fs[curr_dir[:-i]] += size
+        match line.split(" "):
+            case ["dir", _] | ["$", "ls"]:
+                continue
+            case ["$", "cd", ".."]:
+                curr_dir = curr_dir[:-1]
+            case ["$", "cd", "/"]:
+                curr_dir = ("/",)
+            case ["$", "cd", dest]:
+                curr_dir = curr_dir + (dest,)
+            case [size, _]:
+                size = int(size)
+                fs[curr_dir] += size
+                for i in range(1, len(curr_dir)):
+                    fs[curr_dir[:-i]] += size
     unused = 70_000_000 - fs[("/",)]
     need = 30_000_000
-    print(sum(size for size in fs.values() if size <= 100_000))
-    print(next(size for size in sorted(fs.values()) if (unused + size) >= need))
+    sizes = sorted(fs.values())
+    print(sum(s for s in sizes if s <= 100_000))
+    print(next(s for s in sizes if (unused + s) >= need))
 
 
 solve(puzzle_input("7"))
